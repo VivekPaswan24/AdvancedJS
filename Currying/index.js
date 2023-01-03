09c28bd8f935cf00b4abc3105a318492210e8674
VivekPaswan24 <vivek.paswan0@gmail.com>
2023-01-03 20:54:30 +0530

    curring

---
+++ Currying/index.js
@@ -0,0 +1,24 @@
+// let multiply=function(x,y){
+//     console.log(x*y);
+// }
+// By using bind
+
+// let multiplyByTwo =multiply.bind(this,2)
+// multiplyByTwo(3);
+
+// let multiplyByThree =multiply.bind(this,3);
+// multiplyByThree(6);
+
+// By using closures
+
+let multiply=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplyByTwo= multiply(2);
+multiplyByTwo(2);
+
+let multiplyByThree=multiply(3);
+multiplyByThree(3)
